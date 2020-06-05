4512f906b7ea28737c5cd9226c86fd4265bf72a8
David <davidfilipeamorim@gmail.com>
2020-06-05 15:08:50 +0100

    Initial commit

---
+++ backend/src/controllers/DislikeController.js
@@ -0,0 +1,22 @@
+const Dev = require("../models/Dev")
+
+module.exports = {
+    async store(req, res) {
+        const { user } = req.headers
+        const { devId } = req.params
+
+        const loggedDev = await Dev.findById(user)
+        let targetDev = null
+        try {
+            targetDev = await Dev.findById(devId)
+        } catch (err) {
+            return res.status(400).json({ error: "Dev not exists" })
+        }
+
+        loggedDev.dislikes.push(targetDev._id)
+
+        await loggedDev.save()
+
+        return res.json(loggedDev)
+    }
+}
---
+++ backend/src/server.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const routes = require("./routes")
+
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+const connectedUsers = {}
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query
+
+    connectedUsers[user] = socket.id
+})
+
+mongoose.connect(process.env.mongodbCredential, {
+     useNewUrlParser: true
+})
+
+app.use((req, res, next) => {
+    req.io = io
+    req.connectedUsers = connectedUsers
+
+    return next()
+})
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+server.listen(3333)
